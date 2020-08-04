4f39751dfeef0265ecd5a1e4a18a176c8551936b
MrWilllian <willian.dev10@gmail.com>
2020-08-04 16:41:43 -0300

    refactor: add migrations

---
+++ aulas/server/src/database/migrations/03_create_connections.ts
@@ -0,0 +1,19 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('connections', table => {
+    table.increments('id').primary();
+    table.timestamp('created_at').defaultTo(knex.raw('CURRENT_TIMESTAMP')).notNullable();
+
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('connections');
+}

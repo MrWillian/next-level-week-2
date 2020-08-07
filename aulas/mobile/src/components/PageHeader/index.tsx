f387ecf9ecd291be68cef3d6fd234176a32ff0ae
MrWilllian <willian.dev10@gmail.com>
2020-08-07 11:04:05 -0300

    refactor: add Page Header last changes

--- aulas/mobile/src/components/PageHeader/index.tsx
+++ aulas/mobile/src/components/PageHeader/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 import backIcon from '../../assets/images/icons/back.png';
 import logoImg from '../../assets/images/logo.png';
 
 import styles from './styles';
-import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps {
   title: string;
+  headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
   const { navigate } = useNavigation();
 
   function handleGoBack() {
@@ -29,8 +30,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         <Image source={logoImg} resizeMode="contain" />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
 
+        {headerRight}
+      </View>
+
+      {children}
     </View>
   );
 }
